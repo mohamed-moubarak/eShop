87a4aeea0f6344eb7e855e310e3832216657c815
Yomuddahany <moh94hany@gmail.com>
2015-09-28 22:13:19 +0200

    Register page created

---
+++ eShop.php
@@ -0,0 +1,11 @@
+<html>
+<title>eShop</title>
+	<head>
+	</head>
+	<body>
+		<a href="Signin.php">Sign in</a>
+		<a href="Register.php">Register</a>
+		<h1> Description </h1><br>
+		<p>write here</p>
+	</body>	
+</html>
