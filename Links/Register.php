65143237cc3efbc57152daed6301338c3e686072
Mohamed Moubarak <mohamed.moubarak@student.guc.edu.eg>
2015-10-09 01:48:02 +0200

    Final project

--- Links/Register.php
+++ Links/Register.php
@@ -1,4 +1,6 @@
 <?php session_start(); ?>
+<!DOCTYPE HTML> 
+
 <html>
     <head>
         <title>eShop | Registration</title>
@@ -7,186 +9,214 @@
         <link href="../CSS/stylesheet.css" rel="stylesheet">
     </head>
 
-    <body> 
-        <?php
-        // define variables and set to empty values
-        error_reporting(E_ALL);
-        $servername = "localhost";
-        $username = "root";
-        $passwordd = "";
-        $dbname = "eshop";
+    <body>
+          <!-- Navbar and Sidebar -->
+          <?php
+            include('navbar.php');
+          ?>
+            <!--registration page-->
 
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $passwordd, $dbname);
+<?php
+// define variables and set to empty values
+error_reporting(E_ALL);
+$servername = "localhost";
+$username = "root";
+$passwordd = "";
+$dbname = "eShop";
 
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }else
-        {
-        echo "Connected successfully";
-        }
+// Create connection
+$conn = mysqli_connect($servername, $username, $passwordd, $dbname);
+if (!$conn) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
 
-        $firstname=$lastname=$email=$password=$telephone=$address=$avatar=$firstnametmp=$lastnametmp=$emailtmp=$passtmp=$telephonetmp="";
-        $firstnameErr=$lastnameErr=$passwordErr=$telephoneErr=$addressErr=$avatarErr=$emailErr="";
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$firstname=$lastname=$email=$password=$telephone=$address=$avatar=$firstnametmp=$lastnametmp=$emailtmp=$passtmp=$telephonetmp="";
+$firstnameErr=$lastnameErr=$passwordErr=$telephoneErr=$addressErr=$avatarErr=$emailErr="";
 
-            //      First Name
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-           if (empty($_POST["firstname"])) {
-             $firstnameErr = "First Name is required";
-           } else {
-             $firstnametmp = test_input($_POST["firstname"]);
-             // check if name only contains letters and whitespace
-             if (!preg_match("/^[a-zA-Z ]*$/",$firstnametmp)) {
-               $firstnameErr = "Only letters and white space allowed"; 
-             }else{
-              $firstname = test_input($_POST["firstname"]);
-             }
-           }
+    //      First Name
 
-            //       lastname
+   if (empty($_POST["firstname"])) {
+     $firstnameErr = "First Name is required";
+   } else {
+     $firstnametmp = test_input($_POST["firstname"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$firstnametmp)) {
+       $firstnameErr = "Only letters and white space allowed"; 
+     }else{
+      $firstname = test_input($_POST["firstname"]);
+     }
+   }
 
-            if (empty($_POST["lastname"])) {
-             $lastnameErr = "Last Name is required";
-           } else {
-             $lastnametmp = test_input($_POST["lastname"]);
-             // check if name only contains letters and whitespace
-             if (!preg_match("/^[a-zA-Z ]*$/",$lastnametmp)) {
-               $lastnameErr = "Only letters and white space allowed"; 
-             }else{
-              $lastname = test_input($_POST["lastname"]);
-             }
-           }
+    //       lastname
 
-           //       Email
+    if (empty($_POST["lastname"])) {
+     $lastnameErr = "Last Name is required";
+   } else {
+     $lastnametmp = test_input($_POST["lastname"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$lastnametmp)) {
+       $lastnameErr = "Only letters and white space allowed"; 
+     }else{
+      $lastname = test_input($_POST["lastname"]);
+     }
+   }
 
-           if (empty($_POST["email"])) {
-             $emailErr = "Email is required";
-           } else {
-             $emailtmp = test_input($_POST["email"]);
-             // check if e-mail address is well-formed
-             if (!filter_var($emailtmp, FILTER_VALIDATE_EMAIL)) {
-               $emailErr = "Invalid email format"; 
-             }else{
-              $email = test_input($_POST["email"]);
-             }
-           }
+   //       Email
 
-           //       Password
-             
-           if (empty($_POST["password"])) {
-             $passwordErr = "Enter Password";
-           } else {
-             // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-            $passtmp=$_POST["password"];
-             if (!preg_match("#.*^(?=.{9,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$#", $passtmp)){
-              $passwordErr="Password is weak . It must be more than 8 characters, include at least one letter,  one Number and a Capital letter";
-            }else {
-              $password=$_POST["password"];
-            }
-         }
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $emailtmp = test_input($_POST["email"]);
+     // check if e-mail address is well-formed
+     if (!filter_var($emailtmp, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format"; 
+     }else{
+      $email = test_input($_POST["email"]);
+     }
+   }
 
-           if (empty($_POST["telephone"])){
-            $telephoneErr = "Enter Telephone Number";
-           } else {
-             $telephonetmp = test_input($_POST["telephone"]);
-            if (!ctype_digit($telephonetmp)){
-            $telephoneErr = "Enter Numbers Only";
-           }else{
-            $telephone=test_input($_POST["telephone"]);
-           }
-         }
+   //       Password
+     
+   if (empty($_POST["password"])) {
+     $passwordErr = "Enter Password";
+   } else {
+     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    $passtmp=$_POST["password"];
+     if (!preg_match("#.*^(?=.{9,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$#", $passtmp)){
+      $passwordErr="Password is weak . It must be more than 8 characters, include at least one letter,  one Number and a Capital letter";
+    }else {
+      $password=$_POST["password"];
+    }
+ }
 
-           //       Address
-           if (empty($_POST["address"])) {
-             $addressErr = "Address is required";
-           } else {
-             $address = test_input($_POST["address"]);
-           }
+   if (empty($_POST["telephone"])){
+    $telephoneErr = "Enter Telephone Number";
+   } else {
+     $telephonetmp = test_input($_POST["telephone"]);
+    if (!ctype_digit($telephonetmp)){
+    $telephoneErr = "Enter Numbers Only";
+   }else{
+    $telephone=test_input($_POST["telephone"]);
+   }
+ }
+
+   //       Address
+   if (empty($_POST["address"])) {
+     $addressErr = "Address is required";
+   } else {
+     $address = test_input($_POST["address"]);
+   }
 
 
-           if (empty($_POST["avatar"])) {
-             $avatarErr = "Avatar is required";
-           } else {
-             $avatar = test_input($_POST["avatar"]);
-           }
-         
-         
-        }
+   if (empty($_POST["avatar"])) {
+     $avatarErr = "Avatar is required";
+   } else {
+     $avatar = test_input($_POST["avatar"]);
+   }
+ 
+ 
+}
 
-        //////////////////////////////////////////////////////////////Check///////////////////////////////////////////////////
-        function checkPassword($pwd, $errors) {
-            if (strlen($pwd) < 8) {
-                $errors = "Password too short!";
-            }
+//////////////////////////////////////////////////////////////Check///////////////////////////////////////////////////
+function checkPassword($pwd, $errors) {
+    if (strlen($pwd) < 8) {
+        $errors = "Password too short!";
+    }
 
-            if (!preg_match("#[0-9]+#", $pwd)) {
-                $errors = "Password must include at least one number!";
-            }
+    if (!preg_match("#[0-9]+#", $pwd)) {
+        $errors = "Password must include at least one number!";
+    }
 
-            if (!preg_match("#[a-zA-Z]+#", $pwd)) {
-                $errors = "Password must include at least one letter!";
-            }     
+    if (!preg_match("#[a-zA-Z]+#", $pwd)) {
+        $errors = "Password must include at least one letter!";
+    }     
 
-            return $errors;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    return $errors;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        function test_input($data) {
-           $data = trim($data);
-           $data = stripslashes($data);
-           $data = htmlspecialchars($data);
-           return $data;
-        }
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
 
-        ?>
+if (strlen($firstname&&$lastname&&$email&&$password&&$telephone&&$address&&$avatar)>0){
+  $sql = "INSERT INTO User (first_name, last_name, email, password, phone_number, address , avatar)
+  VALUES ('$firstname', '$lastname', '$email', '$password', '$telephone', '$address', '$avatar')";
+ }else{
+  $sql = FALSE;
+ }
+ if(!empty($sql)){
+  if ($conn->query($sql) === TRUE) {
+      
+      $_SESSION["email"] = $email;
+      $conn->close();
+      header("Location: register_success.php");
+    } else {
+      $email="";
+      echo "Email Already Existed";
+      $conn->close();
+    }
+  }
 
-        <h2>Registeration Form</h2>
-        <p><span class="error">* required field.</span></p>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-           First Name: <input type="text" name="firstname" maxlength="40" value="<?php echo $firstname;?>">
-           <span class="error">* <?php echo $firstnameErr;?></span>
-           <br><br>
-           Last Name: <input type="text" name="lastname" maxlength="40" value="<?php echo $lastname;?>">
-           <span class="error">* <?php echo $lastnameErr;?></span>
-           <br><br>
-           E-mail: <input type="text" name="email" maxlength="127" value="<?php echo $email;?>">
-           <span class="error">* <?php echo $emailErr;?></span>
-           <br><br>
-           Password: <input type="password" name="password" maxlength="20" value="<?php echo $password;?>">
-           <span class="error">* <?php echo $passwordErr;?></span>
-           <br><br>
-           Telephone: <input type="text" name="telephone" maxlength="25" value="<?php echo $telephone;?>">
-           <span class="error">* <?php echo $telephoneErr;?></span>
-           <br><br>
-           Address: <input type="text" name="address" maxlength="256" value="<?php echo $address;?>">
-           <span class="error">* <?php echo $addressErr;?></span>
-           <br><br>
-           Avatar: <input type="text" name="avatar" maxlength="256" value="<?php echo $avatar;?>">
-           <span class="error">* <?php echo $avatarErr;?></span>
-           <br><br>
-           <input type="submit" name="submit" value="Submit"> 
-        </form>
+?>
+              
+                <div class="col-md-6">
+                  <h3 class="dark-grey">Registration</h3>
+                  <p><span class="error">* required field.</span></p>
+                  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+                    <div class="form-group col-lg-12">
+                      <label>First Name</label>
+                      <span class="error"> * <?php echo $firstnameErr;?></span>
+                      <input type="text" class="form-control" name="firstname" maxlength="40" value="<?php echo $firstname;?>">
+                    </div>
+                    <div class="form-group col-lg-12">
+                      <label>Last Name</label>
+                      <span class="error"> * <?php echo $lastnameErr;?></span>
+                      <input type="text" class="form-control" name="lastname" maxlength="40" value="<?php echo $lastname;?>">
+                    </div>
+                    <div class="form-group col-lg-6">
+                      <label>Email</label>
+                      <span class="error"> * <?php echo $emailErr;?></span>
+                      <input type="text" class="form-control" name="email" maxlength="127" value="<?php echo $email;?>">
+                    </div>
+                    <div class="form-group col-lg-6">
+                      <label>Password</label>
+                      <span class="error"> * <?php echo $passwordErr;?></span>
+                      <input type="password" class="form-control" name="password" maxlength="20" value="<?php echo $password;?>">
+                    </div>
+                    <div class="form-group col-lg-6">
+                      <label>Telephone</label>
+                      <span class="error"> * <?php echo $telephoneErr;?></span>
+                      <input type="text" class="form-control" name="telephone" maxlength="25" value="<?php echo $telephone;?>">
+                    </div>
+                    <div class="form-group col-lg-6">
+                      <label>Address</label>
+                      <span class="error"> * <?php echo $addressErr;?></span>
+                      <input type="text" class="form-control" name="address" maxlength="256" value="<?php echo $address;?>">
+                    </div>
+                    <div class="form-group col-lg-12">
+                      <label>Avatar</label>
+                      <span class="error"> * <?php echo $avatarErr;?></span>
+                      <input type="text" class="form-control" name="avatar" maxlength="256" value="<?php echo $avatar;?>">
+                    </div>
+                    <div class="form-group col-lg-2 ">
+                      <input type="submit" class="btn btn-primary" name="submit" value="Register"> 
+                    </div>
+                  </form>
+                </div>
 
-        <?php
-
-        $sql = "INSERT INTO Users (First_name, Last_name, Email, Password, Phone_Number, Address , Avatar)
-        VALUES ('$firstname', '$lastname', '$email', '$password', '$telephone', '$address', '$avatar')";
-        
-        
-        if(!empty($conn->query($sql))){
-          $_SESSION["email"] = '$email';
-          mysqli_close($conn);
-          header("Location: ../index.php");
-        }
-
-        
-        ?>
-
-        <!-- Bootstrap core JavaScript -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="../js/bootstrap.min.js"></script>
+      <script src="../js/jquery.js"></script>
+      <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
--- Links/products.php
+++ Links/products.php
@@ -2,90 +2,191 @@
 	<head>
 		<title>eShop | Products</title>
 		<!-- Bootstrap core CSS -->
+		<link href="../CSS/bootstrap-theme.css" rel="stylesheet">
+		<link href="../CSS/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="../CSS/bootstrap.css" rel="stylesheet">
     	<link href="../CSS/bootstrap.min.css" rel="stylesheet">
     	<link href="../CSS/stylesheet.css" rel="stylesheet">
+		<link href="css/simple-sidebar.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
 	</head>
 
 	<body>
-		<div id="wrapper">
-			<!-- Nav Bar -->
-			<nav class="navbar navbar-inverse navbar-static-top">
-	          <div class="container">
-	            <div class="navbar-header">
-	              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	                <span class="sr-only">Toggle navigation</span>
-	                <span class="icon-bar"></span>
-	                <span class="icon-bar"></span>
-	                <span class="icon-bar"></span>
-	              </button>
-	              <a class="navbar-brand" href="#">eShop</a>
-	            </div>
-	            <div id="navbar" class="navbar-collapse collapse">
-	              <ul class="nav navbar-nav">
-	                <li class="active"><a href="#">Home</a></li>
-	                <li><a href="#about">About</a></li>
-	                <li><a href="#contact">Contact</a></li>
-	                <li class="dropdown">
-	                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-	                  <ul class="dropdown-menu">
-	                    <li><a href="#">Action</a></li>
-	                    <li><a href="#">Another action</a></li>
-	                    <li><a href="#">Something else here</a></li>
-	                    <li role="separator" class="divider"></li>
-	                    <li class="dropdown-header">Nav header</li>
-	                    <li><a href="#">Separated link</a></li>
-	                    <li><a href="#">One more separated link</a></li>
-	                  </ul>
-	                </li>
-	                <li><button type="button" class="btn btn-default navbar-btn">Sign in</button></li>
-	              </ul>
-	            </div>
-	          </div>
-	        </nav>
-
-	        <!-- Sidebar -->
-	       	<nav class="navbar navbar-default sidebar" role="navigation"> 
-	       		<div class="container-fluid"> 
-	       			<div class="navbar-header"> 
-	       				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1"> 
-		       				<span class="sr-only">Toggle navigation</span> 
-		       				<span class="icon-bar"></span> 
-		       				<span class="icon-bar"></span> 
-		       				<span class="icon-bar"></span> 
-	       				</button> 
-	       			</div> 
-	       			<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1"> 
-	       				<ul class="nav navbar-nav"> 
-	       					<li class="active">
-	       						<a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a>
-	       					</li> 
-			       			<li class="dropdown"> 
-			       				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuarios
-			       					<span class="caret"></span>
-				       				<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a> 
-				       			<ul class="dropdown-menu forAnimate" role="menu"> 
-				       				<li><a href="{{URL::to('createusuario')}}">Crear</a></li> 
-				       				<li><a href="#">Modificar</a></li> 
-				       				<li><a href="#">Reportar</a></li> 
-				       				<li class="divider"></li> 
-				       				<li><a href="#">Separated link</a></li> 
-				       				<li class="divider"></li> 
-				       				<li><a href="#">Informes</a></li> 
-				       			</ul> 
-			       			</li> 
-	       					<li><a href="#">Libros
-	       						<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-th-list"></span></a></li> 
-	       					<li><a href="#">Tags
-	       						<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-tags"></span></a></li> 
-	       				</ul> 
-	       			</div> 
-	       		</div> 
-	       	</nav>
+		
+		<!-- Navbar and Sidebar -->
+        <?php
+        	session_start();
+        	include 'config.php';
+        	include('unauthenticated_header.php');
+        ?>
 	        <!-- Products Page -->
+	        <div class="col-md-10">
+	           <?php
+
+//current URL of the Page. cart_update.php redirects back to this URL
+$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+?>
+
+<!-- View Cart Box Start -->
+<?php
+if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
+{
+    echo '<div class="cart-view-table-front" id="view-cart">';
+    echo '<h3>Your Shopping Cart</h3>';
+    echo '<form method="post" action="cart_update.php">';
+    echo '<table width="100%"  cellpadding="6" cellspacing="0">';
+    echo '<tbody>';
+
+    $total =0;
+    $b = 0;
+    foreach ($_SESSION["cart_products"] as $cart_itm)
+    {
+        $product_name = $cart_itm["product_name"];
+        $product_qty = $cart_itm["product_qty"];
+        $product_price = $cart_itm["product_price"];
+        $PID= $cart_itm["PID"];
+        $bg_color = ($b++%2==1) ? 'odd' : 'even'; //zebra stripe
+        echo '<tr class="'.$bg_color.'">';
+        echo '<td>Qty <input type="text" size="2" maxlength="2" name="product_qty['.$PID.']" value="'.$product_qty.'" /></td>';
+        echo '<td>'.$product_name.'</td>';
+        echo '<td><input type="checkbox" name="remove_code[]" value="'.$PID.'" /> Remove</td>';
+        echo '</tr>';
+        $subtotal = ($product_price * $product_qty);
+        $total = ($total + $subtotal);
+    }
+    echo '<td colspan="4">';
+    echo '<button type="submit">Update</button><a href="view_cart.php" class="button">Checkout</a>';
+    echo '</td>';
+    echo '</tbody>';
+    echo '</table>';
+    
+    $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
+    echo '</form>';
+    echo '</div>';
+
+}
+?>
+
+	        	<h3 class="dark-grey col-md-offset-1">Products</h3> 
+	        	<div class="table-responsive page-header col-md-offset-1">          
+ 					<table class="table">
+					    <tbody>
+					        <tr>
+					        <!-- View Cart Box End -->
+
+							<!-- Products List Start -->
+<?php
+$results = $mysqli->query("SELECT PID  , name as product_name, picture_url as product_img_name , stock,  price FROM products");
+if($results){ 
+$products_item = '';
+//fetch results set as object and output HTML
+$counter = 0;
+while($obj = $results->fetch_object())
+{
+	if($obj->stock>0) {
+		$counter++;
+	if ($counter%4 == 0) {
+		$products_item .= <<<EOT
+	<td data-field="price" align="center">
+	<form method="post" action="cart_update.php">
+	<img class="img-thumbnail" src="{$obj->product_img_name}" alt="Generic placeholder image" width="200" height="200"> <br/>{$obj->product_name}<br/> Price: {$currency}{$obj->price} 
+	<fieldset>
+
+	<label>
+		<span>Quantity</span>
+		<input type="text" size="2" maxlength="2" name="product_qty" value="1" />
+	</label>
+	
+	</fieldset>
+	<input type="hidden" name="PID" value="{$obj->PID}" />
+	<input type="hidden" name="type" value="add" />
+	<div align="center"><button type="submit" class="add_to_cart">Add</button></div>
+	</div>
+	</form>
+	</td>
+	</tr><tr>
+
+EOT;
+	}
+	else {
+$products_item .= <<<EOT
+
+	<td data-field="price" align="center">
+	<form method="post" action="cart_update.php">
+	<img class="img-thumbnail" src="{$obj->product_img_name}" alt="Generic placeholder image" width="200" height="200"> <br/>{$obj->product_name}<br/> Price: {$currency}{$obj->price} 
+
+	<fieldset>
+
+	<label>
+		<span>Quantity</span>
+		<input type="text" size="2" maxlength="2" name="product_qty" value="1" />
+	</label>
+	
+	</fieldset>
+	<input type="hidden" name="PID" value="{$obj->PID}" />
+	<input type="hidden" name="type" value="add" />
+	<div align="center"><button type="submit" class="add_to_cart">Add</button></div>
+	</div>
+	</form>
+	</td>
+
+EOT;
+}}
+	else {
+	$counter++;
+	if ($counter%4 == 0) {
+		$products_item .= <<<EOT
+	<td data-field="price" align="center">
+	<form method="post" action="cart_update.php">
+	<img class="img-thumbnail" src="{$obj->product_img_name}" alt="Generic placeholder image" width="200" height="200"> <br/>{$obj->product_name}<br/> Price: {$currency}{$obj->price}
+
+
+	<input type="hidden" name="PID" value="{$obj->PID}" />
+	<input type="hidden" name="type" value="add" />
+	<div align="center" style="color: red;">Out of Stock!</div>
+	</div>
+	</form>
+	</td>
+	</tr><tr>
+
+EOT;
+	}
+	else {
+$products_item .= <<<EOT
+
+	<td data-field="price" align="center">
+	<form method="post" action="cart_update.php">
+	<img class="img-thumbnail" src="{$obj->product_img_name}" alt="Generic placeholder image" width="200" height="200"> <br/>{$obj->product_name}<br/> Price: {$currency}{$obj->price} 
+
+
+	<input type="hidden" name="PID" value="{$obj->PID}" />
+	<input type="hidden" name="type" value="add" />
+	<div align="center" style="color: red;">Out of Stock!</div>
+	</div>
+	</form>
+	</td>
+
+EOT;
+}}
+}
+
+echo $products_item . '</tr>';
+}
+?>    
+					        
+
+					    </tbody>
+					</table> 
+				</div>	
+		    </div>
+
+	        
 
 		<!-- Bootstrap core JavaScript -->
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    	<script src="../js/jquery.js"></script>
     	<script src="../js/bootstrap.min.js"></script>
+
 	</body>
 
 </html>
